1a902ad847da20c14f1a5d10a8dbf78d38b05d00
root <rzrka1998@gmil.com>
2025-04-14 23:33:46 +0300

    state

---
+++ Source/SevenFlower/Private/Characters/Alyse/AlysePlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright QueueBitGames
+
+
+#include "Characters/Alyse/AlysePlayerState.h"
+
+AAlysePlayerState::AAlysePlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/SevenFlower/Public/Characters/Alyse/AlysePlayerState.h
@@ -0,0 +1,19 @@
+// Copyright QueueBitGames
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AlysePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEVENFLOWER_API AAlysePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAlysePlayerState();
+};
